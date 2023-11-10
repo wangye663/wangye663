65f8f8aa54d7fb29eaefa3b60f70b9209a433c21
wangye663 <150289957+wangye663@users.noreply.github.com>
2023-11-11 07:50:56 +0800

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,10 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
+# Replace 'YOUR_API_TOKEN' with the API token obtained from BotFather
+TOKEN = '6431056796:AAGqVBt44TA-r_ozQyMqBmTR9av_OBkv-Sc'
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Hello, I am your Telegram bot.')
+
+def echo(update: Update, context: CallbackContext
